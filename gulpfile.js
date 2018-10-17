7fdd5dac64b5c7c8c3706e98ca0e7c944574c495
Snater <git@snater.com>
2018-10-17 18:31:48 +0200

    Have Gulp clean directories before copying

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
 var gulp = require('gulp');
+var del = require('del');
+var merge = require('merge-stream');
 var sass = require('gulp-sass');
 var importOnce = require('node-sass-import-once');
 
 var root = 'web/themes/custom/relaunch2018/';
 
+gulp.task('clean', function() {
+  return del([
+    'web/themes/custom/relaunch2018/lib/bootstrap',
+    'web/themes/custom/relaunch2018/lib/jquery.easing',
+    'web/themes/custom/relaunch2018/lib/popper.js',
+    'web/themes/custom/relaunch2018/lib/fonts/barlow'
+  ]);
+});
+
 gulp.task('copy', function() {
-  gulp.src(['node_modules/bootstrap/**/*'])
-  .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/bootstrap'));
+  return merge(
+    gulp.src(['node_modules/bootstrap/**/*'])
+    .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/bootstrap')),
 
-  gulp.src(['node_modules/jquery.easing/**/*'])
-  .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/jquery.easing'));
+    gulp.src(['node_modules/jquery.easing/**/*'])
+    .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/jquery.easing')),
 
-  gulp.src(['node_modules/popper.js/**/*'])
-  .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/popper.js'));
+    gulp.src(['node_modules/popper.js/**/*'])
+    .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/popper.js')),
 
-  return gulp.src(['node_modules/typeface-barlow/**/*'])
-  .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/fonts/barlow'));
+    gulp.src(['node_modules/typeface-barlow/**/*'])
+    .pipe(gulp.dest('web/themes/custom/relaunch2018/lib/fonts/barlow'))
+  );
 });
 
 gulp.task('scss', function() {
@@ -26,4 +39,4 @@ gulp.task('scss', function() {
   .pipe(gulp.dest(root + 'css'))
 });
 
-gulp.task('default', gulp.series('copy', 'scss'), function() {});
+gulp.task('default', gulp.series('clean', 'copy', 'scss'), function() {});
