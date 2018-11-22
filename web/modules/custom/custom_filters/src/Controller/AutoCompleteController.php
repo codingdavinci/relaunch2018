fe562e4a3afc0b482881bad9594f13ca93a61b8f
Snater <git@snater.com>
2018-11-22 16:58:58 +0100

    #43: Auto-complete input box for selecting institution

---
+++ web/modules/custom/custom_filters/src/Controller/AutoCompleteController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\custom_filters\Controller;
+
+use Drupal\Component\Utility\Tags;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Defines a route controller for entity auto-complete form elements.
+ */
+class AutoCompleteController extends ControllerBase {
+
+  /**
+   * Handler for auto-complete request.
+   *
+   * @param Request $request
+   * @param string $field_name
+   * @param string $count
+   * @return JsonResponse
+   */
+  public function handleAutoComplete(Request $request, $field_name, $count) {
+    $results = [];
+
+    // Get the typed string from the URL, if it exists.
+    if ($input = $request->query->get('q')) {
+      $typedString = Tags::explode($input);
+      $typedString = mb_strtolower(array_pop($typedString));
+
+      $options = [];
+
+      if ($field_name === 'institution') {
+        $options = $this->getFilteredInstitutions($typedString);
+      }
+
+      $results = [];
+
+      foreach ($options as $value => $label) {
+        $results[] = [
+          'value' => $value,
+          'label' => $label,
+        ];
+        if (--$count === 0) {
+          break;
+        }
+      }
+
+    }
+
+    return new JsonResponse($results);
+  }
+
+  /**
+   * @param string $typedString
+   * @return array
+   */
+  protected function getFilteredInstitutions($typedString) {
+    $options = [];
+    $institutionUserIds = \Drupal::entityQuery('user')
+      ->condition('status', 1)
+      ->condition('roles', 'institution')
+      ->execute();
+    $users = User::loadMultiple($institutionUserIds);
+
+    /** @var User $user */
+    foreach ($users as $user) {
+      $institutionName = $user->get('field_institution_name')->value;
+      $options[$institutionName] = $institutionName;
+    }
+
+    sort($options);
+
+    return array_filter(
+      array_unique($options),
+      function($institutionName) use ($typedString) {
+        return strpos(mb_strtolower($institutionName), $typedString) !== FALSE;
+      }
+    );
+  }
+
+}
