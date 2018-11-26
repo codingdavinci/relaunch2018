93fc2852d4a8efc03b83444ac6ea8315a358dbd3
Snater <git@snater.com>
2018-11-26 21:45:29 +0100

    #61: Applied admin theme to user pages

---
+++ web/modules/custom/custom_rendering/src/Routing/RouteSubscriber.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\custom_rendering\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('entity.user.canonical')) {
+      $route->setOption('_admin_route', TRUE);
+    }
+  }
+
+}
