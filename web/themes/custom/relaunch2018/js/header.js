d4005782577235f46e2355eb7124f17b9c6665dc
Snater <git@snater.com>
2018-12-13 23:01:57 +0100

    #3: Added basic tiled header on event page

--- web/themes/custom/relaunch2018/js/header.js
+++ web/themes/custom/relaunch2018/js/header.js
@@ -9,7 +9,7 @@
 
   $(function() {
     var $tileContainer = $('.tiles');
-    var originalTileSize = 214;
+    var originalTileSize = $tileContainer.hasClass('tiles--frontpage') ? 214 : 280;
     var tileSize = Math.sqrt(originalTileSize * originalTileSize * 2);
 
     // Top left edge of the unrotated tile in even columns:
@@ -18,8 +18,9 @@
     // Top left edge of the unrotated tile in odd columns:
     var odd = {top: even.top - tileSize / 2, left: even.left + tileSize / 2};
 
+    var frontSideTilesCount = $tileContainer.hasClass('tiles--frontpage') ? 31 : 20;
     var backSideTileCount = 9;
-    var maxLines = 8;
+    var maxLines = $tileContainer.hasClass('tiles--frontpage') ? 8 : 6;
 
     var html = [];
     var frontSide_i = 0;
@@ -48,7 +49,7 @@
         html.push(''
           + '<div class="header-tile" style="left:' + l + 'px;top:' + t + 'px;">'
           + '<div class="flipper">'
-          + '<div class="front cdv-tiles'+ (frontSide_i % 20 + 1) + '"></div>'
+          + '<div class="front cdv-tiles'+ (frontSide_i % frontSideTilesCount + 1) + '"></div>'
           + '<div class="back code-tile' + backSide_i + '"></div>'
           + '</div>'
           + '</div>');
