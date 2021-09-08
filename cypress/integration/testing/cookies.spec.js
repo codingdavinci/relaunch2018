e2ea7f641354cc699ee3d01d29694075d3502622
jschoenerstedt <82206841+jschoenerstedt@users.noreply.github.com>
2021-09-08 11:14:49 +0200

    Changes cypress test

--- cypress/integration/testing/cookies.spec.js
+++ cypress/integration/testing/cookies.spec.js
@@ -3,17 +3,17 @@ context('Cookiebanner', () => {
   it('Prüft generelle Funktionalität', () => {
     cy.visit('https://test.codingdavinci.de/');
     cy.clearCookies()
-    // cy.getCookies().should('be.empty')
-    // cy.get('.agree-button').click();
-    // cy.getCookie('cookie-agreed').should('have.property', 'value', '2')
-    // cy.wait(1000)
-    // cy.clearCookies()
-    // cy.wait(1000)
-    // cy.visit('https://test.codingdavinci.de/');
-    // cy.clearCookies()
-    // cy.wait(1000)
-    // cy.getCookies().should('be.empty')
-    // cy.get('.decline-button').click();
-    // cy.getCookie('cookie-agreed').should('have.property', 'value', '0')
+    cy.getCookies().should('be.empty')
+    cy.get('.agree-button').click();
+    cy.getCookie('cookie-agreed').should('have.property', 'value', '2')
+    cy.wait(1000)
+    cy.clearCookies()
+    cy.wait(1000)
+    cy.visit('https://test.codingdavinci.de/');
+    cy.clearCookies()
+    cy.wait(1000)
+    cy.getCookies().should('be.empty')
+    cy.get('.decline-button').click();
+    cy.getCookie('cookie-agreed').should('have.property', 'value', '0')
   })
 })
