e9918d365c389f3ef2d5dddaceacea9c62a92644
Michael Büchner <mbuechner@users.noreply.github.com>
2021-11-01 13:49:53 +0100

    Update drupal-maintenance.sh

--- scripts/drupal-maintenance.sh
+++ scripts/drupal-maintenance.sh
@@ -4,10 +4,10 @@ CACHEREBUILD_ON_STARTUP="${CACHEREBUILD_ON_STARTUP:-no}";
 
 if [ "$UPDATEDB_ON_STARTUP" = "yes" ]; then
   echo "Start Drupal Update DB...";
-  /var/www/html/vendor/bin/drush --root /var/www/html/web updatedb;
+  /var/www/html/vendor/bin/drush -y --root /var/www/html/web updatedb;
 fi;
 
 if [ "$CACHEREBUILD_ON_STARTUP" = "yes" ]; then
   echo "Start Drupal Cache Rebuild...";
-  /var/www/html/vendor/bin/drush --root /var/www/html/web cache-rebuild;
+  /var/www/html/vendor/bin/drush -y --root /var/www/html/web cache-rebuild;
 fi;
