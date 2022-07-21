8d091227d14345edab6f0ed26d2d4348e6870b96
Michael Büchner <m.buechner@dnb.de>
2022-07-21 10:36:54 +0200

    Drupal 9.4.3; fixed Redis config bug

--- scripts/drupal-maintenance.sh
+++ scripts/drupal-maintenance.sh
@@ -2,6 +2,7 @@
 UPDATEDB_ON_STARTUP="${UPDATEDB_ON_STARTUP:-no}";
 CACHEREBUILD_ON_STARTUP="${CACHEREBUILD_ON_STARTUP:-no}";
 REDIS_MAXMEMORY="${REDIS_MAXMEMORY:-1gb}";
+USE_REDIS="${USE_REDIS:-no}";
 
 if [ "$UPDATEDB_ON_STARTUP" = "yes" ]; then
   echo "Start Drupal Update DB...";
@@ -13,5 +14,16 @@ if [ "$CACHEREBUILD_ON_STARTUP" = "yes" ]; then
   /var/www/html/vendor/bin/drush -y --root /var/www/html/web cache-rebuild;
 fi;
 
-/usr/bin/redis-cli config set maxmemory "${REDIS_MAXMEMORY}";
-/usr/bin/redis-cli config set maxmemory-policy volatile-lru;
+if [ "$USE_REDIS" = "yes" ]; then
+  # check if Redis is ready for 180 sec.
+  for i in $(seq 1 180); do
+    if (/usr/bin/redis-cli ping 2>&1 /dev/null); then
+      /usr/bin/redis-cli config set maxmemory "${REDIS_MAXMEMORY}";
+      /usr/bin/redis-cli config set maxmemory-policy volatile-lru;
+      echo "Set Redis configuration successfully.";
+      break;
+    fi;
+    echo "Redis is not ready (since $i sec.)...";
+    sleep 1.0;
+  done
+fi;
